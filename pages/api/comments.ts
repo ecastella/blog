f28bcb86a90a8a372b61da65f668bf71e189d19b
ecastella <ecastella6@gmail.com>
2022-08-24 14:53:02 -0700

    Further updated comments.ts

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient, gql } from "graphql-request";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphqlAPI = 'process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT';
 const graphcmsToken = process.env.GRAPHCMS_TOKEN;
 
 
