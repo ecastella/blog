01495351d065f686c3a38a89ff24fdd0a736625b
Evan Castella <ecastella6@gmail.com>
2022-09-22 14:40:19 -0700

    :D

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.scss';
 import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app'
+
+import { AppProps } from 'next/app';
 import React, { useEffect, useState } from 'react';
-import { Layout } from '../components'; 
+import Layout from '../components'; 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
+  return (
+  
   <Layout>
   <Component {...pageProps} />
   </Layout>
+  
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import { PostCard, Categories, Widget } from '../components';
-import { getPosts } from '../services'
+import { getPosts } from '../services';
 
 
-export default function NextPage ({ posts }) {
+export default function Home ({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-        <Head>
-          <title>My Blog</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <title>My Blog</title>
+        <link rel="icon" href="/favicon.ico" />
      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
       <div className="lg:col-span-8 col-span-1">
-        {posts.map((post) => <PostCard post={post.node}  key={post.title}/>)}
+        {posts.map((post, index) => (
+        <PostCard post={post.node}  key={index}/>
+        ))}
         </div>
       <div className="lg:col-span-4 col-span-1">
         <div className="lg:sticky relative top-8">
@@ -28,10 +26,9 @@ export default function NextPage ({ posts }) {
 
 export async function getStaticProps() {
  const posts = (await getPosts()) || [];
-
-  return {
-    props: { posts }
+return {
+    props: { posts },
   }
-}
+};
 
 
