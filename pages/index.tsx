95025919faf06f5b9f12bce243757d61d456c539
Evan Castella <ecastella6@gmail.com>
2022-09-25 12:47:59 -0700

    Updated index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ export default function Home ({ posts }: any) {
         <link rel="icon" href="/favicon.ico" />
      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
       <div className="lg:col-span-8 col-span-1">
-        {posts.map((post: { node: any; }, index: Key | null | undefined) => (
-          <PostCard post={post.node}  key={index}/>
+        {posts.map((post: { node: any; title: any;}) => (
+          <PostCard post={post.node}  key={post.title}/>
         ))}
         </div>
       <div className="lg:col-span-4 col-span-1">
